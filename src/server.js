60748d799a75353c929c5aa5daf01f0bbf72be10
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-07-21 23:44:46 -0300

    Middleware + randomUUID.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 import http from "node:http";
 import { Database } from "./database.js";
+import { randomUUID } from "node:crypto";
+import { convertDataToJson } from "./middlewares/convertDataToJson.js";
 
 const port = 3000;
 const host = "localhost";
 
 const database = new Database();
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  await convertDataToJson(req, res);
 
   if (url && url === "/tasks") {
     if (method === "GET") {
@@ -17,30 +21,20 @@ const server = http.createServer((req, res) => {
     }
 
     if (method === "POST") {
-      let body = [];
+      const { title, description, isDone } = req.body;
 
-      req
-        .on("data", (chunk) => {
-          body.push(chunk);
-        })
-        .on("end", () => {
-          body = JSON.parse(Buffer.concat(body).toString());
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        isDone,
+      };
 
-          const { title, description, isDone } = body;
-
-          const task = {
-            id: 2,
-            title,
-            description,
-            isDone,
-          };
-
-          const data = database.insert("tasks", task);
-          res.writeHead(200).end(`Task: ${data.title} registrada com sucesso`);
-        });
+      const data = database.insert("tasks", task);
+      res.writeHead(200).end(`Task: ${data.title} registrada com sucesso`);
     }
   } else {
-    res.end("URL não encontrada").statusCode(404);
+    res.statusCode(404).end("URL não encontrada");
   }
 });
 
