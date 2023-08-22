e68759fe5094c327c03359d05eee3c7c88d8d6e4
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-08-21 21:58:02 -0300

    Import tasks via csv file reading.

--- src/utils/read-csv-file.js
+++ src/utils/read-csv-file.js
@@ -1,3 +1,37 @@
-import { routes } from "../routes";
+import fs from "node:fs";
+import { parse } from "csv-parse";
 
-const route = routes.filter((route) => route.method === "POST");
+// get the file's path
+const filePath = import.meta.url;
+const csvPath = new URL("../../tasks.csv", filePath);
+
+// reads the file
+const readFile = fs.createReadStream(csvPath);
+
+const csvParse = parse({
+  delimiter: ",", // delimites each field
+  from_line: 2, // starts reading from the 2nd line
+  skipEmptyLines: true,
+  trim: true,
+});
+
+async function readAndSaveTasks() {
+  const lines = readFile.pipe(csvParse);
+
+  for await (const line of lines) {
+    const [title, description] = line;
+
+    await fetch("http://localhost:3000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        description,
+      }),
+    });
+  }
+}
+
+readAndSaveTasks();
